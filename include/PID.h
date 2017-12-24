5fb78397f565249924009592b2bda3ed30aea201
Anton Pozharskiy <apozharski@gmail.com>
2017-12-24 14:04:35 -0500

    initial function headers

--- include/PID.h
+++ include/PID.h
@@ -16,5 +16,29 @@ typedef struct PID
   float prev_error;
   float integral;
   float stable_delta;
-}
+  float output;
+} PID;
+
+PID init_PID(float (*get_real)(),float (*get_target)());
+
+void start_PID(PID *pid);
+
+float get_PID_output(PID *pid);
+
+void set_kp(PID *pid, float P);
+
+void set_ki(PID *pid, float I);
+
+void set_kd(PID *pid, float D);
+
+void set_delta(PID *pid, float delta);
+
+float get_error(PID *pid);
+
+float get_integral(PID *pid);
+
+float get_deriv(PID *pid);
+
+bool is_stable(PID *pid);
+
 #endif /* REXAPI_H_ */
